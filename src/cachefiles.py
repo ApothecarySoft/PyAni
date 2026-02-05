5c8039d9e0894afe524dcd357b5c2edcb494f453
apothecarysoft <rrcaseyr@gmail.com>
2026-02-05 11:22:02 -0500

    hide internal functions

--- src/cachefiles.py
+++ src/cachefiles.py
@@ -6,36 +6,36 @@ import constants
 import re
 
 
-def getTodayDateStamp():
+def _getTodayDateStamp():
     return str(date.today()).replace("-", "")
 
 
-def compareDateStamps(stamp1, stamp2=None, delta=constants.OLD_DATA_THRESHOLD):
+def _compareDateStamps(stamp1, stamp2=None, delta=constants.OLD_DATA_THRESHOLD):
     if not stamp2:
-        stamp2 = getTodayDateStamp()
+        stamp2 = _getTodayDateStamp()
     return abs(int(stamp1) - int(stamp2)) <= delta
 
 
-def generateDataFileNameForUser(userName: str):
-    return f"{sanitizeUserName(userName=userName)}-{getTodayDateStamp()}-list.json"
+def _generateDataFileNameForUser(userName: str):
+    return f"{_sanitizeUserName(userName=userName)}-{_getTodayDateStamp()}-list.json"
 
 
 def saveUserDataFile(userName: str, entries: list):
-    with open(generateDataFileNameForUser(userName=userName), "w") as file:
+    with open(_generateDataFileNameForUser(userName=userName), "w") as file:
         json.dump(entries, file)
 
 
-def sanitizeUserName(userName: str):
+def _sanitizeUserName(userName: str):
     return re.sub(r'[^a-zA-Z0-9_-]', '', userName)
 
 
 def latestValidUserFileOrNew(userName: str, clean=True):
-    fileNames = glob(f"{sanitizeUserName(userName=userName)}-*-list.json")
+    fileNames = glob(f"{_sanitizeUserName(userName=userName)}-*-list.json")
     latestValidFileName = None
     latestValidDateStamp = None
     for fileName in fileNames:
-        dateStamp = extractDateStampFromFileName(fileName=fileName)
-        if compareDateStamps(dateStamp):
+        dateStamp = _extractDateStampFromFileName(fileName=fileName)
+        if _compareDateStamps(dateStamp):
             if not latestValidDateStamp or dateStamp > latestValidDateStamp:
                 if clean and latestValidFileName:
                     os.remove(latestValidFileName)
@@ -45,10 +45,10 @@ def latestValidUserFileOrNew(userName: str, clean=True):
                 os.remove(fileName)
         elif clean:
             os.remove(fileName)
-    return latestValidFileName or generateDataFileNameForUser(userName=userName)
+    return latestValidFileName or _generateDataFileNameForUser(userName=userName)
 
 
-def extractDateStampFromFileName(fileName):
+def _extractDateStampFromFileName(fileName):
     return int(fileName.split("-")[-2])
 
 
